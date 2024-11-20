e309c56740c18211f4fdd4b66e80a36a0635a2a7
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-11-20 17:34:45 +0700

    update logout api

--- apis/auth-api/logout.js
+++ apis/auth-api/logout.js
@@ -3,7 +3,7 @@ import axios from "@/apis/api-constant";
 export const logout = async () => {
     const url = "/api/auth/logout";
     try {
-        const response = await axios.get(url);
+        const response = await axios.post(url);
         return response.data;
     } catch (error) {
         console.error("Logout failed:", error);
