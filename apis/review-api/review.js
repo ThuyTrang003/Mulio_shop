8cf797214957febd5a0c305dd76073f8f53dc72a
qmy03 <nguyenquynhmy.qng03@gmail.com>
2024-12-03 15:28:49 +0700

    update review

--- apis/review-api/review.js
+++ apis/review-api/review.js
@@ -12,19 +12,31 @@ export const getReviewsByBase = async (skuBase) => {
     }
 };
 
+// export const postReviews = async (payload) => {
+//     payload = {
+//         rating: payload.rating,
+//         comment: payload.comment,
+//         images: payload.images,
+//     };
+//     try {
+//         const url =
+//             "http://localhost:8080/api/products/reviews/${payload.productId}";
+//         const response = await axios.post(url, payload);
+//         return response.data.data;
+//     } catch (error) {
+//         console.error("Evaluate product failed:", error);
+//         throw error.response.data;
+//     }
+// };
 export const postReviews = async (payload) => {
-    payload = {
-        rating: payload.rating,
-        comment: payload.comment,
-        images: payload.images,
-    };
+    const { rating, comment, images, productId } = payload;
     try {
-        const url =
-            "http://localhost:8080/api/products/reviews/${payload.productId}";
-        const response = await axios.post(url, payload);
+        const url = `http://localhost:8080/api/products/reviews/${productId}`;
+        const response = await axios.post(url, { rating, comment, images });
         return response.data.data;
     } catch (error) {
         console.error("Evaluate product failed:", error);
         throw error.response.data;
     }
 };
+
